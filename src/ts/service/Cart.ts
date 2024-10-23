728975feb4be29511a0cba947d31d0269ea30ce9
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-10-23 14:31:31 +0300

    add file

---
+++ src/ts/service/Cart.ts
@@ -0,0 +1,25 @@
+import Buyable from '../domain/Buyable';
+
+export default class Cart {
+    private _items: Buyable[] = [];
+
+    add(item: Buyable): void {
+        this._items.push(item);
+    }
+
+    get items(): Buyable[] {
+        return [...this._items];
+    }
+
+    sum(): number {
+        return this._items.reduce((sum, el) => sum += el.price, 0)
+    }
+
+    discountSum(discount: number): number {
+        return this.sum() * (1 - (discount / 100));
+    }
+
+    deleteItem(id: number): void {
+        this._items = this._items.filter(item => item.id !== id)
+    }
+}
